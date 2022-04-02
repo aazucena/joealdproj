d3288368c449b0856a002e7c7c3836be6a263cd4
Joseph Jackson <joseph.jackson@uleth.ca>
2022-04-02 17:52:19 -0600

    more fixes

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ import express from 'express'
 import { getOffset, emptyOrRows } from './helper.js'
 import router from './routes/table.js'
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: true, }))
-app.get("/", (req, res) => res.json({ message: "ok" }))
-app.use("/joealdproj", router)
+app.use(express.urlencoded({ extended: true, }));
+app.get("/", (req, res) => res.json({ message: "ok" }));
+app.use("/joealdproj", router);
 
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500
-  console.error(err.message, err.stack)
-  res.status(statusCode).json({ message: err.message })
-  return
-})
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+});
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`) )
+app.listen(port, () => console.log(`listening at http://localhost:${port}`) );
