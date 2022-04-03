bc2159090fd081bc74fa3224ca3a53866dca5fc0
Joseph Jackson <joseph.jackson@uleth.ca>
2022-04-02 18:22:15 -0600

    found fix?

--- services/db.js
+++ services/db.js
@@ -3,7 +3,7 @@ import config from '../config.js'
 
 export default async function query(sql, params) {
   const connection = await mysql.createConnection(config.db);
-  const [results, ] = await connection.execute(sql, params);
+  const results = await connection.execute(sql, params);
 
   return results;
 }
